4e59a7d4b6bda28ac83a6e76e8f36c4b5a3420eb
alfadlims <alfadli20001@mail.unpad.ac.id>
2023-11-12 13:21:05 +0700

    fix: import Subscription class

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Subscription;
 
 class PaymentController extends Controller
 {
